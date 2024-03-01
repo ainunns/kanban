98b6d960d4579179cab1db995ff05202459f8baa
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2024-03-01 21:51:55 +0700

    feat: add update ticket backlog

--- src/components/board/TicketBoard.jsx
+++ src/components/board/TicketBoard.jsx
@@ -1,33 +1,54 @@
 import "./../../styles/components/board.css";
 
 import * as React from "react";
-import { FaCalendar } from "react-icons/fa";
+import { FaCalendar, FaEdit } from "react-icons/fa";
 
 import ModalDetail from "../../app/components/ModalDetail";
+import ModalEditTicket from "../../app/components/ModalEdit";
 import { randomColor, showFormattedDate } from "../../lib/helper";
 import Chips from "../Chips";
 
 export default function BoardTicket({ data, dataModal, setDataModal }) {
   const tags = data.tags;
-  const [open, setOpen] = React.useState(false);
-
+  const [openDetail, setOpenDetail] = React.useState(false);
+  const [openEdit, setOpenEdit] = React.useState(false);
   return (
     <>
-      <ModalDetail open={open} setOpen={setOpen} dataModal={dataModal} />
+      <ModalEditTicket
+        open={openEdit}
+        setOpen={setOpenEdit}
+        dataModal={dataModal}
+      />
+      <ModalDetail
+        open={openDetail}
+        setOpen={setOpenDetail}
+        dataModal={dataModal}
+      />
       <div className="board__content__ticket">
-        <div className="board__content__ticket__tags">
-          {tags.map((tag) => (
-            <Chips color={randomColor()} key={tag}>
-              {tag}
-            </Chips>
-          ))}
+        <div className="board__content__ticket__header">
+          <div className="board__content__ticket__tags">
+            {tags.map((tag) => (
+              <Chips color={randomColor()} key={tag}>
+                {tag}
+              </Chips>
+            ))}
+          </div>
+          <button
+            className="board__content__ticket__edit"
+            onClick={() => {
+              setDataModal(data);
+              setOpenEdit(true);
+            }}
+          >
+            <FaEdit size={16} fontWeight={400} />
+          </button>
         </div>
         <div className="board__content__ticket__detail">
           <h3
             className="board__content__ticket__title"
             onClick={() => {
               setDataModal(data);
-              setOpen(true);
+              setOpenDetail(true);
             }}
           >
             {data.title}
